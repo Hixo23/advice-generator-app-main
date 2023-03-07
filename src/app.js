b8f97812109820dbd2fc031d8ba12dac4f70c814
Hiko <hixohixo32@gmail.com>
2023-03-07 16:36:24 +0100

    advice-generator-app

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const adviceText = document.querySelector('#adviceText');
+const btn = document.querySelector('#diceBtn');
+const adviceId = document.querySelector('#adviceId');
+
+const init = () => {
+getAdvice()
+}
+
+const getAdvice = async () => {
+   const response = await fetch('https://api.adviceslip.com/advice');
+   const json = await response.json();
+
+   console.log(json)
+   adviceId.textContent = json.slip.id
+   adviceText.textContent = json.slip.advice
+}
+
+btn.addEventListener('click', (event) => {
+    getAdvice()
+})
+
+
+init()
+
